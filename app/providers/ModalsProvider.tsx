f8e3981371bd397f04cab17de20a51b6ab5f58b1
Andres Camacho Aguilar <andcamachoa@gmail.com>
2023-04-19 10:26:30 -0600

    import-rentmodal-fix

--- app/providers/ModalsProvider.tsx
+++ app/providers/ModalsProvider.tsx
@@ -2,7 +2,7 @@
 import LoginModal from '@/app/components/modals/LoginModal';
 import RegisterModal from '@/app/components/modals/RegisterModal';
 import SearchModal from '@/app/components/modals/SearchModal';
-import RentModal from '../app/components/modals/RentModal';
+import RentModal from '@/app/components/modals/RentModal';
 
 
 
